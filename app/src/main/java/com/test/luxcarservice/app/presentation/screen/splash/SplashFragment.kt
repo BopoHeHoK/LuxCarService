8f0ad650eed0a60885f851457a9aaf43e61754c7
Dmitry Korolyov <dimakorolev2030@gmail.com>
2023-06-04 22:12:49 +0300

    first commit322

--- app/src/main/java/com/test/luxcarservice/app/presentation/screen/splash/SplashFragment.kt
+++ app/src/main/java/com/test/luxcarservice/app/presentation/screen/splash/SplashFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import com.test.luxcarservice.R
 import com.test.luxcarservice.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
@@ -25,5 +30,10 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(3000)
+            Navigation.findNavController(view)
+                .navigate(R.id.action_splashFragment_to_rootFragment)
+        }
     }
 }
